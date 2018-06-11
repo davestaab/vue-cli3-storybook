98623bcf830c0923bed2fea1ffac4e11ab9590ee
Dave Staab <dave.staab@cadre5.com>
2018-06-11 09:27:22 -0400

    adding storybook

---
+++ .storybook/config.js
@@ -0,0 +1,10 @@
+import { configure } from "@storybook/vue";
+
+import "vue";
+
+function loadStories() {
+  // You can require as many stories as you need.
+  require("../src/components/Button.story");
+}
+
+configure(loadStories, module);
