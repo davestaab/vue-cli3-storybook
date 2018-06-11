d3ab64debdc2d9514c1424f810420ae362e54109
Dave Staab <davestaab@gmail.com>
2018-06-11 12:19:40 -0400

    removed extra config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,6 @@
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = (baseConfig, env, defaultConfig) => {
-  //   updateWebpackConfig(defaultConfig);
   defaultConfig.plugins.push(new VueLoaderPlugin());
-
-  defaultConfig.resolve.extensions.push("vue$", ".vue/dist/vue.esm.js");
-  defaultConfig.resolve.extensions = [".js", ".json", ".vue", ".jsx"];
-
   return defaultConfig;
 };
